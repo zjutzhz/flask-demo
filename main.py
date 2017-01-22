7d5b2e789aac0e7122d5740c145c5c8d767c8c9a
Hz Zheng <zjutzhz@gmail.com>
2017-01-23 00:08:45 +0800

    first

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from config import DevConfig
+
+app=Flask(__name__)
+
+app.config.from_object(DevConfig)
+
+@app.route('/')
+def root():
+    return "<h1>Hello world</h1>"
+
+if __name__ == "__main__":
+    app.run()
